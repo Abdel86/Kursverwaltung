119783ff08eef2b22fcc112f5236628568920449
tonioanantharaj <anantharajtonio@gmail.com>
2020-08-15 21:19:39 +0200

    Tonio

---
+++ portFolio/admin/verwalter.php
@@ -0,0 +1,29 @@
+<?php
+require('person.php');
+include('database.php');
+$db = new Database();
+/**
+ * [Diese Klasse repräsentiert einen Verwalter, die Klasse erbt die Eigenschaften einer Person]
+ */
+ class Verwalter extends Person
+{
+  protected $verwalterID;
+  protected $verwalterUsername;
+  protected $verwalterEmail;
+  protected $password;
+  protected $kursID;
+  protected $pruefungsID;
+  protected $session;
+
+
+  
+
+  
+
+}
+
+
+
+ ?>
+
+
