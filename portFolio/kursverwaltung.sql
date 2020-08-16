5db4c05da0d6f84ac6ebcda36f990de9f9109e68
Abdel86 <a.aouam@hotmail.de>
2020-08-16 15:36:25 +0200

    Update kursverwaltung.sql

--- portFolio/kursverwaltung.sql
+++ portFolio/kursverwaltung.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 11. Aug 2020 um 20:40
+-- Erstellungszeit: 16. Aug 2020 um 15:35
 -- Server-Version: 10.4.11-MariaDB
 -- PHP-Version: 7.2.31
 
@@ -39,9 +39,8 @@ CREATE TABLE `dozent` (
 --
 
 INSERT INTO `dozent` (`dozentID`, `dozentName`, `dozentEmail`, `password`) VALUES
-(1, 'Kressler', '', ''),
-(2, 'Hinrich', '', ''),
-(3, 'Lenkau', '', '');
+(12, 'Andreas', '', ''),
+(13, 'Hessler', '', '');
 
 -- --------------------------------------------------------
 
@@ -52,9 +51,12 @@ INSERT INTO `dozent` (`dozentID`, `dozentName`, `dozentEmail`, `password`) VALUE
 CREATE TABLE `kurs` (
   `kursID` int(11) NOT NULL,
   `kursNummer` int(11) NOT NULL,
+  `kursName` varchar(255) NOT NULL,
   `kursBeginn` date NOT NULL,
   `kursEnde` date NOT NULL,
   `teilnehmerAnzahl` int(11) NOT NULL,
+  `dozentID` int(11) NOT NULL,
+  `kosten` decimal(10,0) NOT NULL,
   `kursCreated` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -62,23 +64,14 @@ CREATE TABLE `kurs` (
 -- Daten für Tabelle `kurs`
 --
 
-INSERT INTO `kurs` (`kursID`, `kursNummer`, `kursBeginn`, `kursEnde`, `teilnehmerAnzahl`, `kursCreated`) VALUES
-(1, 12452, '2020-08-12', '2020-08-19', 9, '2020-08-08 23:00:12'),
-(2, 1221, '2020-08-18', '2020-08-19', 8, '2020-08-08 23:02:47'),
-(3, 1221, '2020-08-19', '2020-08-20', 5, '2020-08-08 23:03:22'),
-(4, 22211, '2020-08-22', '2020-08-14', 11, '2020-08-08 23:04:28'),
-(5, 5555555, '2020-08-12', '2020-08-11', 10, '2020-08-08 23:06:42'),
-(6, 666666, '2020-08-05', '2020-08-04', 9, '2020-08-08 23:07:56'),
-(7, 1010101, '2020-08-05', '0000-00-00', 122, '2020-08-10 19:43:18'),
-(8, 0, '0000-00-00', '0000-00-00', 0, '2020-08-11 13:26:00'),
-(9, 0, '0000-00-00', '0000-00-00', 0, '2020-08-11 13:27:15'),
-(10, 0, '0000-00-00', '0000-00-00', 0, '2020-08-11 14:10:39'),
-(11, 0, '0000-00-00', '0000-00-00', 0, '2020-08-11 14:13:38'),
-(12, 1111111111, '2020-07-29', '0000-00-00', 10, '2020-08-11 14:18:24'),
-(13, 1111111111, '2020-08-20', '0000-00-00', 10, '2020-08-11 14:18:56'),
-(14, 1111111111, '2020-08-06', '2020-08-26', 100, '2020-08-11 14:19:10'),
-(15, 1986, '2020-08-13', '2020-08-21', 120, '2020-08-11 14:39:59'),
-(16, 88888888, '2020-07-29', '2020-08-20', 124, '2020-08-11 16:51:06');
+INSERT INTO `kurs` (`kursID`, `kursNummer`, `kursName`, `kursBeginn`, `kursEnde`, `teilnehmerAnzahl`, `dozentID`, `kosten`, `kursCreated`) VALUES
+(67, 1986, 'HTML', '2020-08-06', '2020-08-13', 10, 12, '0', '2020-08-15 13:28:22'),
+(68, 681425, 'CSS', '2020-08-26', '2020-09-26', 10, 12, '0', '2020-08-15 13:28:46'),
+(69, 12452, 'Abdessamad ds', '2020-07-29', '2020-07-29', 10, 12, '0', '2020-08-15 13:30:13'),
+(70, 123456, 'HTML', '2020-08-06', '2020-08-13', 12, 12, '0', '2020-08-15 15:05:19'),
+(71, 112233, 'OOP', '2020-08-14', '2020-08-15', 2, 12, '0', '2020-08-15 15:09:20'),
+(72, 198622, 'HTML', '2020-08-12', '2020-08-22', 20, 12, '0', '2020-08-15 17:50:15'),
+(73, 198622, 'HTML', '2020-08-05', '2020-08-22', 12, 12, '0', '2020-08-15 19:16:32');
 
 -- --------------------------------------------------------
 
@@ -89,9 +82,12 @@ INSERT INTO `kurs` (`kursID`, `kursNummer`, `kursBeginn`, `kursEnde`, `teilnehme
 CREATE TABLE `pruefung` (
   `pruefungID` int(11) NOT NULL,
   `pruefungNummer` int(11) NOT NULL,
+  `pruefungName` varchar(255) NOT NULL,
   `pruefungBeginn` date NOT NULL,
   `pruefungEnde` date NOT NULL,
   `teilnehmerAnzahl` int(11) NOT NULL,
+  `dozentID` int(11) NOT NULL,
+  `kosten` decimal(10,0) NOT NULL,
   `pruefungCreated` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -99,12 +95,11 @@ CREATE TABLE `pruefung` (
 -- Daten für Tabelle `pruefung`
 --
 
-INSERT INTO `pruefung` (`pruefungID`, `pruefungNummer`, `pruefungBeginn`, `pruefungEnde`, `teilnehmerAnzahl`, `pruefungCreated`) VALUES
-(1, 22211, '2020-08-05', '2020-08-20', 8, '2020-08-08 23:19:58'),
-(2, 1111111111, '2020-08-19', '2020-08-20', 100, '2020-08-11 14:27:59'),
-(3, 222222, '2020-08-20', '2020-08-13', 10, '2020-08-11 14:30:48'),
-(4, 0, '2020-09-04', '2020-08-13', 152, '2020-08-11 14:32:28'),
-(5, 1986, '2020-08-15', '2020-08-19', 120, '2020-08-11 14:41:00');
+INSERT INTO `pruefung` (`pruefungID`, `pruefungNummer`, `pruefungName`, `pruefungBeginn`, `pruefungEnde`, `teilnehmerAnzahl`, `dozentID`, `kosten`, `pruefungCreated`) VALUES
+(13, 131452, 'Java', '2020-07-29', '2020-08-04', 6, 12, '50', '2020-08-15 10:20:48'),
+(14, 111111, 'dsds', '2020-08-04', '2020-09-09', 6, 12, '100', '2020-08-15 10:59:19'),
+(15, 101010, 'Projekt', '2020-08-19', '2020-08-30', 20, 12, '0', '2020-08-15 13:25:24'),
+(16, 1111111111, 'Zoubayr Aouam', '2020-08-05', '2020-08-11', 11, 12, '0', '2020-08-15 13:30:30');
 
 -- --------------------------------------------------------
 
@@ -116,8 +111,20 @@ CREATE TABLE `teilnehmer` (
   `teilnehmerID` int(11) NOT NULL,
   `teilnehmerName` varchar(100) NOT NULL,
   `teilnehmerEmail` varchar(100) NOT NULL,
-  `password` varchar(100) NOT NULL
+  `password` varchar(100) NOT NULL,
+  `kursID` int(11) DEFAULT NULL,
+  `pruefungID` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Daten für Tabelle `teilnehmer`
+--
+
+INSERT INTO `teilnehmer` (`teilnehmerID`, `teilnehmerName`, `teilnehmerEmail`, `password`, `kursID`, `pruefungID`) VALUES
+(5, 'Abdessamad', 'a@b.c', '', 67, NULL),
+(6, 'Abdessamad Aouam', 'a.aouam@hotmail.de', '', 67, NULL),
+(7, 'Abdessamad Aouam', 'a.aouam@hotmail.de', '', 67, NULL),
+(8, 'Abdessamad Aouam', 'a.aouam@hotmail.de', '', 67, NULL);
 
 -- --------------------------------------------------------
 
@@ -149,19 +156,23 @@ ALTER TABLE `dozent`
 --
 ALTER TABLE `kurs`
   ADD PRIMARY KEY (`kursID`),
-  ADD KEY `TeilnehmerID` (`teilnehmerAnzahl`);
+  ADD KEY `TeilnehmerID` (`teilnehmerAnzahl`),
+  ADD KEY `dozentID` (`dozentID`);
 
 --
 -- Indizes für die Tabelle `pruefung`
 --
 ALTER TABLE `pruefung`
-  ADD PRIMARY KEY (`pruefungID`);
+  ADD PRIMARY KEY (`pruefungID`),
+  ADD KEY `dozentID` (`dozentID`);
 
 --
 -- Indizes für die Tabelle `teilnehmer`
 --
 ALTER TABLE `teilnehmer`
-  ADD PRIMARY KEY (`teilnehmerID`);
+  ADD PRIMARY KEY (`teilnehmerID`),
+  ADD KEY `kursID` (`kursID`),
+  ADD KEY `pruefungID` (`pruefungID`);
 
 --
 -- Indizes für die Tabelle `verwalter`
@@ -179,31 +190,54 @@ ALTER TABLE `verwalter`
 -- AUTO_INCREMENT für Tabelle `dozent`
 --
 ALTER TABLE `dozent`
-  MODIFY `dozentID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `dozentID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT für Tabelle `kurs`
 --
 ALTER TABLE `kurs`
-  MODIFY `kursID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `kursID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=74;
 
 --
 -- AUTO_INCREMENT für Tabelle `pruefung`
 --
 ALTER TABLE `pruefung`
-  MODIFY `pruefungID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `pruefungID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT für Tabelle `teilnehmer`
 --
 ALTER TABLE `teilnehmer`
-  MODIFY `teilnehmerID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `teilnehmerID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT für Tabelle `verwalter`
 --
 ALTER TABLE `verwalter`
   MODIFY `verwalterID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints der exportierten Tabellen
+--
+
+--
+-- Constraints der Tabelle `kurs`
+--
+ALTER TABLE `kurs`
+  ADD CONSTRAINT `kurs_ibfk_1` FOREIGN KEY (`dozentID`) REFERENCES `dozent` (`dozentID`);
+
+--
+-- Constraints der Tabelle `pruefung`
+--
+ALTER TABLE `pruefung`
+  ADD CONSTRAINT `pruefung_ibfk_1` FOREIGN KEY (`dozentID`) REFERENCES `dozent` (`dozentID`);
+
+--
+-- Constraints der Tabelle `teilnehmer`
+--
+ALTER TABLE `teilnehmer`
+  ADD CONSTRAINT `teilnehmer_ibfk_1` FOREIGN KEY (`kursID`) REFERENCES `kurs` (`kursID`),
+  ADD CONSTRAINT `teilnehmer_ibfk_2` FOREIGN KEY (`pruefungID`) REFERENCES `pruefung` (`pruefungID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
